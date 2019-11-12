b9ab95f8cfcd2373961b6c967502d7ff0232279d
Wenwei Xiong <wxiong@Wenweis-MBP.local.paradigm4.com>
2019-11-12 17:42:58 -0500

    Add ArrayOpV18$load_file function. Reorganize ArrayOp$join related tests into separate fiels.

--- R/array_op_v18.R
+++ R/array_op_v18.R
@@ -26,7 +26,51 @@ ArrayOpV18 <- R6::R6Class("ArrayOpV18",
   
   # Public ----------------------------------------------------------------------------------------------------------
   public = list(
-    
+    #' @description 
+    #' Get an CustomizedOp that generates AFL for aio_input
+    #'
+    #' @param filepath A single file path
+    #' @param template An ArrayOp sub-class used as an template for fields detection and matching
+    #' @param aio_settings Customized settings of aio_input
+    #' @param field_conversion If NULL (default), use template's field type to convert aio_input attributes; Or provide
+    #' a list for customized field conversion
+    #' @param field_types Instead of using a template, explicitly specify a field_types list. Cannot be used with template
+    #'
+    #' @return CustomizedOp ready to be used in a WriteOp as the dataset
+    #' @export
+    load_file = function(filepath, aio_settings = list(), field_conversion = NULL){
+      
+      # if(methods::hasArg('template')){
+      #   assert(inherits(template, c('ArrayOpBase')),
+      #     "get_aio_op: unknown template class '%s'. Must be ArrayOpBase sub-class", class(template))
+      #   fieldTypes = template$get_field_types(template$dims_n_attrs)
+      # } else {
+      #   fieldTypes = field_types
+      # }
+      
+      fieldTypes = self$get_field_types(self$dims_n_attrs)
+      
+      # Populate aio settings
+      aio_settings = c(list(path = filepath, num_attributes = length(fieldTypes)), aio_settings)
+      settingItems = mapply(private$to_setting_item_str, names(aio_settings), aio_settings)
+      
+      # cast raw attributes
+      castedItems = mapply(function(ft, index, name){
+        fmt = if(ft == 'string') "%s" else paste0(ft, "(%s)")
+        # If there is customized field conversion, use it
+        attrName = sprintf("a%s", index)
+        if(!is.null(field_conversion) && !is.null(field_conversion[[name]]))
+          # If customized field conversion defined for 'name', then use it
+          gsub('@', attrName, field_conversion[[name]])  # Replace all @ occurences in template
+        else # Otherwise just directly 'cast' it to the right data type if needed.
+          sprintf(fmt, attrName)
+      }, fieldTypes, 1:length(fieldTypes) - 1, names(fieldTypes))
+      
+      aioExpr = afl(afl_join_fields(settingItems) %aio_input% NULL)
+      applyExpr = afl(aioExpr %apply% afl_join_fields(names(fieldTypes), castedItems))
+      projectedExpr = afl(applyExpr %project% names(fieldTypes))
+      return(self$create_new(projectedExpr, metaList = list()))
+    }
   )
 )
 
--- tests/testthat/v18/__source.R
+++ tests/testthat/v18/__source.R
@@ -1,4 +1,5 @@
 context('V18-specific tests')
 newArrayOp = ArrayOpV18$new
 source(relative_path('test_array_op.R'), local = TRUE)
+source(relative_path('test_array_op_join.R'), local = TRUE)
 
