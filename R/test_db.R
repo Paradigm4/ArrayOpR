8a03f36fb940a3ee2745832cabdbc2acc5b45f41
wenweiP4 <wxiong@paradigm4.com>
2019-11-16 00:45:29 -0500

    Add tests for ArrayOp $where and $match functions.

--- R/test_db.R
+++ R/test_db.R
@@ -46,6 +46,8 @@ ScidbTest = R6::R6Class(
         self$create_namespace()
       private$log_job(self$test_simple_load(), "Test simple loading file into array")
       private$log_job(self$test_load_with_auto_increment_id(), "Test loading file with auto-increment id")
+      private$log_job(self$test_where(), "Test ArrayOp$where(...)")
+      private$log_job(self$test_match(), "Test ArrayOp$match(...)")
     }
     ,
     create_namespace = function() {
@@ -109,6 +111,52 @@ ScidbTest = R6::R6Class(
         assert(min(resultRepo$vid) == 10)     
         assert(max(resultRepo$vid) == 15)     
        })
+    }
+    ,
+    test_where = function() {
+      V = self$repo$get_alias_schema('V1')
+      try({
+        filtered = self$repo$query(V$where(chrom == 1))
+        assert(nrow(filtered) == 2)
+        assert(all(filtered$chrom == 1))
+        assert(all(filtered$extra == c('variant1', 'variant2')))
+        
+        filtered = self$repo$query(V$where(chrom != 1 & extra %like% '.*3.*'))
+        assert(nrow(filtered) == 2)
+        assert(all(filtered$chrom == c(2, 3)))
+        assert(all(filtered$extra == c('variant3', 'variant-chr3-1')))
+      })
+    }
+    ,
+    test_match = function() {
+      V = self$repo$get_alias_schema('V1')
+      df = data.frame(chrom=c(1,2))
+      built = V$build_new(df)
+      try({
+        for(result in list(
+          self$repo$query(V$match(df, op_mode = 'filter'))
+          , self$repo$query(V$match(built, op_mode = 'cross_between'))
+          , self$repo$query(V$match(df, op_mode = 'filter'))
+          # TODO Change lower/upper bound to cross_between mode
+          #, self$repo$query(V$match(data.frame(chrom_low=1, chrom_hi=2), op_mode = 'filter', 
+          #                           lower_bound = 'chrom_low', upper_bound = 'chrom_hi'))
+          , self$repo$query(V$match(built, op_mode = 'cross_between', 
+                                    lower_bound = list(chrom = 'chrom-3'),
+                                    upper_bound = list(chrom = 'chrom+0')
+                                    ))
+        )){
+          assert(nrow(result) == 4)
+          assert(all(result$chrom == 1 || result$chrom == 2))
+          assert(base::setequal(result$extra, c('variant1', 'variant2', 'variant3', 'variant4')))
+        }
+        browser()
+        assert(nrow(self$repo$query(V$match(built, op_mode = 'cross_between', 
+            lower_bound = list(chrom = 'chrom-3'), upper_bound = list(chrom = 'chrom') ))) == 4)
+        
+        # TODO: lower/upper bound is ignored if there is matching field
+        # assert(nrow(self$repo$query(V$match(built, op_mode = 'cross_between', 
+        #     lower_bound = list(chrom = 'chrom-3'), upper_bound = list(chrom = 'chrom+1') ))) == 5)
+      })
     }
   )
 )
