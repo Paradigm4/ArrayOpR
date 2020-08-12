84a888c25ababb048d376f0eb70a899b79c0ed0b
wenweip4 <wxiong@paradigm4.com>
2020-08-12 21:06:33 +0000

    Modify test_mutate.R so it doesn't need scidb namespace feature

--- tests/testthat/db_array_op/test_mutate.R
+++ tests/testthat/db_array_op/test_mutate.R
@@ -1,13 +1,11 @@
-context("Mutate array content/cells")
+context("ArrayOp: mutate")
 
-
-MutateArray = testNS$create_local_arrayop(
-  'mutate_array',
+schemaTemplate = conn$array_op_from_schema_str(
   "<lower:string COMPRESSION 'zlib', upper:string, f_int32:int32, f_int64:int64, f_bool: bool, f_double: double> 
       [da=0:*:0:*; db=0:*:0:*]"
 )
 
-DfMutateArray = data.frame(
+ArrayContent = data.frame(
   da=0:4, db=10:14,
   lower = letters[1:5], 
   upper = LETTERS[1:5],
@@ -17,107 +15,104 @@ DfMutateArray = data.frame(
   f_double = c(3.14, 2.0, NA, 0, -99)
 )
 
+RefArray = conn$
+  array_op_from_df(ArrayContent, schemaTemplate)$
+  change_schema(schemaTemplate)$
+  persist(.gc = FALSE)
 
-test_that("Array content is correctly populated", {
-  testNS$reset_array_with_content(MutateArray, DfMutateArray)
-  
-  # Validate the target array
-  # sort by lower, needed because scidb doesn't keep the order of inserted data
-  dfFromDb = dplyr::arrange(MutateArray$to_df(), lower)
-  expect_equal(dfFromDb, DfMutateArray)
-})
+assert_df_match = function(result_df, expected_df) {
+  expect_equal(
+    dplyr::arrange(result_df, da), # scidb doesn't gurantee result data frame ordering
+    expected_df # locally filtered data frames follow default ordering
+  )
+}
+
+# Mutate by expression ----
 
 test_that("Mutate an array by expression", {
-  testNS$reset_array_with_content(MutateArray, DfMutateArray)
-  target = MutateArray
-  df = DfMutateArray
-  
-  mutated = target$filter(is_null(f_bool))$
-    mutate(list('f_bool'='iif(f_double > 1, true, false)', 'upper'="'mutated'"))
-  mutated$update(target)$execute()
-  dfFromDb = dplyr::arrange(target$to_df(), lower)
-  expect_equal(dfFromDb, dplyr::mutate(df, upper=c('A', 'mutated', 'C', 'mutated', 'E'), f_bool=c(T,T,F,F,F)))
+  assert_df_match(
+    RefArray$
+      filter(is_null(f_bool))$
+      mutate(list('f_bool'='iif(f_double > 1, true, false)', 'upper'="'mutated'", 'f_int32' = "strlen(lower)"))$
+      to_df(),
+    ArrayContent %>% 
+      dplyr::filter(is.na(f_bool)) %>%
+      dplyr::mutate("f_bool" = f_double > 1, "upper" = "mutated", "f_int32" = nchar(lower))
+  )
 })
 
-test_that("Mutate an array by another array", {
+# Mutate by another arrayOp ----
+
+test_that("mutate: key:1-attr, update_fields: 2 attrs", {
+  mutateDataSource = ArrayContent %>% 
+    dplyr::filter(da %% 2 == 0) %>% 
+    dplyr::mutate(f_int32 = 1:3, upper = 'changed')
   
-  mutate_by_one_attr = function(){
-    testNS$reset_array_with_content(MutateArray, DfMutateArray, recreate = F)
-    mutateDataSource = MutateArray$build_new(
-      DfMutateArray %>% dplyr::filter(da %% 2 == 0) %>% dplyr::select(lower, upper) %>% dplyr::mutate(f_int32 = 1:3)
-    )
-    
-    MutateArray$
-      mutate(mutateDataSource, keys = c('lower'), updated_fields = 'f_int32')$
-      update(MutateArray)$
-      execute()
-  }
-  
-  mutate_by_two_attrs = function(){
-    testNS$reset_array_with_content(MutateArray, DfMutateArray, recreate = F)
-    mutateDataSource = MutateArray$build_new(
-      DfMutateArray %>% dplyr::filter(da %% 2 == 0) %>% dplyr::select(lower, upper) %>% dplyr::mutate(f_int32 = 1:3)
-    )
-    
-    MutateArray$
-      mutate(mutateDataSource, keys = c('lower', 'upper'), updated_fields = 'f_int32')$
-      update(MutateArray)$
-      execute()
-    
-    validate_result()
-  }
-  
-  mutate_by_one_dim = function(){
-    testNS$reset_array_with_content(MutateArray, DfMutateArray, recreate = F)
-    mutateDataSource = MutateArray$build_new(
-      DfMutateArray %>% dplyr::filter(da %% 2 == 0) %>% dplyr::select(da) %>% dplyr::mutate(f_int32 = 1:3)
-    )
-    MutateArray$
-      mutate(mutateDataSource, keys = c('da'), updated_fields = 'f_int32')$
-      update(MutateArray)$
-      execute()
-    
-    validate_result()
-  }
-  
-  mutate_by_two_dims = function(){
-    testNS$reset_array_with_content(MutateArray, DfMutateArray, recreate = F)
-    mutateDataSource = MutateArray$build_new(
-      DfMutateArray %>% dplyr::filter(da %% 2 == 0) %>% dplyr::select(da, db) %>% dplyr::mutate(f_int32 = 1:3)
-    )
-  
-    MutateArray$
-      mutate(mutateDataSource, keys = c('da', 'db'), updated_fields = 'f_int32')$
-      update(MutateArray)$
-      execute()
-    
-    validate_result()
-  }
-  
-  mutate_by_dim_n_attr = function(){
-    testNS$reset_array_with_content(MutateArray, DfMutateArray, recreate = F)
-    mutateDataSource = MutateArray$build_new(
-      DfMutateArray %>% dplyr::filter(da %% 2 == 0) %>% dplyr::select(db, lower) %>% dplyr::mutate(f_int32 = 1:3)
-    )
-    
-    MutateArray$
-      mutate(mutateDataSource, keys = c('lower', 'db'), updated_fields = 'f_int32')$
-      update(MutateArray)$
-      execute()
-    validate_result()
-  }
-  
-  validate_result = function(){
-    dfFromDb = dplyr::arrange(MutateArray$to_df(), lower)
-    expect_equal(dfFromDb, DfMutateArray %>% dplyr::mutate(f_int32 = c(1,-4,2,-2,3)))
-  }
-  
-  mutate_by_one_attr()
-  mutate_by_two_attrs()
-  mutate_by_one_dim()
-  mutate_by_two_dims()
-  mutate_by_dim_n_attr()
-  
-  testNS$cleanup_after_each_test()
+  assert_df_match(
+    RefArray$mutate(
+      conn$array_op_from_df(mutateDataSource %>% dplyr::select(lower, upper, f_int32)), 
+      keys = c('lower'), updated_fields = c('f_int32', 'upper')
+    )$to_df(),
+    mutateDataSource
+  )
 })
+
+test_that("mutate: key:2-attrs, update_fields: 1 attr", {
+  mutateDataSource = ArrayContent %>% 
+    dplyr::filter(da %% 2 == 0) %>% 
+    dplyr::mutate(f_int32 = 1:3)
+  
+  dataArray = conn$array_op_from_df(mutateDataSource %>% dplyr::select(lower, upper, f_int32), schemaTemplate)
+  
+  assert_df_match(
+    RefArray$mutate(
+      dataArray, keys = c('lower', 'upper'), updated_fields = c('f_int32')
+    )$to_df(),
+    mutateDataSource
+  )
+})
+
+test_that("mutate: key:1 dim, update_fields: 1 attr", {
+  mutateDataSource = ArrayContent %>% 
+    dplyr::filter(da %% 2 == 0) %>% 
+    dplyr::mutate(f_int32 = 1:3)
+  dataArray = conn$array_op_from_df(mutateDataSource %>% dplyr::select(da, f_int32), schemaTemplate)
+  
+  assert_df_match(
+    RefArray$mutate(
+      dataArray, keys = c('da'), updated_fields = c('f_int32')
+    )$to_df(),
+    mutateDataSource
+  )
+})
+
+test_that("mutate: key:2 dims, update_fields: 1 attr", {
+  mutateDataSource = ArrayContent %>% 
+    dplyr::filter(da %% 2 == 0) %>% 
+    dplyr::mutate(f_int32 = 1:3)
+  dataArray = conn$array_op_from_df(mutateDataSource %>% dplyr::select(da, db, f_int32), schemaTemplate)
+  
+  assert_df_match(
+    RefArray$mutate(
+      dataArray, keys = c('da', 'db'), updated_fields = c('f_int32')
+    )$to_df(),
+    mutateDataSource
+  )
+})
+
+test_that("mutate: key: 1 dim + 1 attr, update_fields: 2 attrs", {
+  mutateDataSource = ArrayContent %>% 
+    dplyr::filter(da %% 2 == 0) %>% 
+    dplyr::mutate(f_int32 = 1:3, upper = "changed")
+  dataArray = conn$array_op_from_df(mutateDataSource %>% dplyr::select(db, lower, f_int32, upper), schemaTemplate)
+  
+  assert_df_match(
+    RefArray$mutate(
+      dataArray, keys = c('lower', 'db'), updated_fields = c('f_int32', 'upper')
+    )$to_df(),
+    mutateDataSource
+  )
+})
+
+RefArray$remove_self()
 
